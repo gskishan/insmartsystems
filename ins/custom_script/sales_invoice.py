768ef685644a7015a59b6c644c220a05bc18c5f7
asifp.ext <asifp.ext@elastic.run>
2022-06-06 14:10:53 +0530

    Solved item not updated

--- ins/custom_script/sales_invoice.py
+++ ins/custom_script/sales_invoice.py
@@ -7,11 +7,16 @@ def before_save(doc, method=None):
 	set_name(doc)
 
 def set_name(doc):
-		series = re.findall(r'\d+',doc.name)[0]
-		month=re.findall(r'\d+',utils.today())[1]
-		st_year = re.findall(r'\d+',frappe.defaults.get_user_default("year_start_date"))[0][-2::]
-		ed_year = re.findall(r'\d+',frappe.defaults.get_user_default("year_end_date"))[0][-2::]
-		vertical=doc.vertical
-		type = doc.type
-		doc.name = st_year+ed_year+type+month+vertical+series
-		return doc.name
+		old_doc = doc.get_doc_before_save()
+		if old_doc == None:
+			#print(f"""\n\n\n\n{doc.name}\n\\n\n""")
+			series = re.findall(r'\d+',doc.name)[0]
+			month=re.findall(r'\d+',utils.today())[1]
+			st_year = re.findall(r'\d+',frappe.defaults.get_user_default("year_start_date"))[0][-2::]
+			ed_year = re.findall(r'\d+',frappe.defaults.get_user_default("year_end_date"))[0][-2::]
+			vertical=doc.vertical
+			type = doc.type
+
+			for data in doc.items:
+				data.parent= st_year+ed_year+type+month+vertical+series
+			doc.name = st_year+ed_year+type+month+vertical+series
