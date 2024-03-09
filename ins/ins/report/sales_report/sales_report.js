9565e767387f4fbf365a4d611de6a2e00204faf1
darachennarao <150008320+darachennarao@users.noreply.github.com>
2024-03-09 16:34:29 +0530

    Update sales_report.js

--- ins/ins/report/sales_report/sales_report.js
+++ ins/ins/report/sales_report/sales_report.js
@@ -7,8 +7,7 @@ frappe.query_reports["Sales Report"] = {
 			fieldname:"customer",
 			label: __("Customer"),
 			fieldtype: "Link",
-			options: "Customer",
-			reqd: 1
+			options: "Customer"
 		},
 		{
 			fieldname:"from_date",
