7938d32967e27e34e5638e55f2b7936a7c97a1b9
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-14 13:49:48 +0530

    Update sales_invoice.py

--- ins/custom_script/sales_invoice.py
+++ ins/custom_script/sales_invoice.py
@@ -2,73 +2,32 @@ from frappe import utils
 import re
 import frappe
 
-@frappe.whitelist()
-def update_oldrecord():
-    sql="""SELECT 
-  name,
-    company,
-    YEAR(posting_date) AS invoice_year,
-    ROW_NUMBER() OVER (PARTITION BY company, YEAR(posting_date) ORDER BY creation) AS order_number
-FROM 
-    `tabSales Invoice`
-ORDER BY 
-    company, creation;"""
-    for d in frappe.db.sql(sql,as_dict=True):
-        si=frappe.get_doc("Sales Invoice",d.name)
-        si.db_set("sequence",0)
 
-def get_year(posting_date):
-    import datetime
-    date_string = str(posting_date)
-    date_object = datetime.datetime.strptime(date_string, '%Y-%m-%d')
-    year = date_object.year
-    return year
-
-def validate(self, method=None):
-    if self.is_new() and 'param' in self.naming_series:
-        try:
-            sql = """SELECT sequence
-                    FROM `tab{0}`
-                    WHERE company=%s AND sequence IS NOT NULL and  YEAR(creation) =%s and naming_series like %s
-                    ORDER BY creation DESC
-                    LIMIT 1""".format(self.doctype)
-        
-            last_count = frappe.db.sql(sql, (self.company,get_year(self.posting_date),self.naming_series), as_dict=False)
-            last_count = last_count[0][0] if last_count else None
-            if last_count is not None:
-                self.sequence = last_count + 1
-            else:
-                self.sequence = 1	
-        except Exception as e:
-            frappe.log_error(f"Error setting  sequence: {str(e)}")
-
-def autoname(doc, method=None):
-    set_name(doc)
+def before_save(doc, method=None):
+	set_name(doc)
 
 def set_name(doc):
-    if 'param' in doc.naming_series:
-        year=frappe.defaults.get_global_default('fiscal_year')
-        month = re.findall(r'\d+', utils.today())[1]
-        vertical = doc.vertical
-        type = doc.type
-        company_code = ""
-        if doc.company == "insmart Systems":
-            company_code = "10000"
-        elif doc.company == "insmart Systems India Private Limited":
-            company_code = "20000"
-        elif doc.company == "OIA TECHNOLOGIES PRIVATE LIMITED":
-            company_code = '30000'
-        sql = """SELECT MAX(sequence)
-                    FROM `tab{0}`
-                    WHERE company=%s AND sequence IS NOT NULL""".format(doc.doctype)
-        max_icv = frappe.db.sql(sql, (doc.company,), as_dict=False)[0][0]
-        if max_icv is not None:
-            sequence = max_icv + 1
-        else:
-            sequence = 1
-        last_number=int(company_code) + sequence
-    
-        if last_number == 0:
-            last_number = int(company_code) + 1
+		old_doc = doc.get_doc_before_save()
+		if old_doc == None and 'param'in doc.naming_series:
+			#print(f"""\n\n\n\n{doc.name}\n\\n\n""")
+			series = re.findall(r'\d+',doc.name)[0]
+			month=re.findall(r'\d+',utils.today())[1]
+			st_year = re.findall(r'\d+',frappe.defaults.get_user_default("year_start_date"))[0][-2::]
+			ed_year = re.findall(r'\d+',frappe.defaults.get_user_default("year_end_date"))[0][-2::]
+			vertical=doc.vertical
+			type = doc.type
 
-        doc.name = year + type + month + vertical + str(last_number)
+			for data in doc.items:
+				company = ""
+				if doc.company == "insmart Systems":
+					company = 1
+				
+				elif doc.company == "insmart Systems India Private Limited":
+					company = 2
+
+				
+				elif doc.company == "OIA TECHNOLOGIES PRIVATE LIMITED":
+					company = 3
+					
+				data.parent= st_year+ed_year+type+month+vertical+company+series
+			doc.name = st_year+ed_year+type+month+vertical+company+series
