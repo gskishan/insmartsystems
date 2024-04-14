41d2e03ce976bedc2cda21b5460817d68a3fcbe5
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-14 18:34:17 +0530

    Update sales_invoice.py

--- ins/custom_script/sales_invoice.py
+++ ins/custom_script/sales_invoice.py
@@ -46,7 +46,6 @@ def autoname(doc, method=None):
 
 def set_name(doc):
     if 'param' in doc.naming_series:
-        frappe.errprint("in")
         year=frappe.defaults.get_global_default('fiscal_year')
         month = re.findall(r'\d+', utils.today())[1]
         vertical = doc.vertical
